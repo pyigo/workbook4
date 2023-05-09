1d2cbaed0f40dd6adc25c1fd89d29d555a95f41a
paulette <paulette.yigo@gmail.com>
2023-05-09 10:41:16 -0400

    exercise1 complete

---
+++ objectScripts/label_maker.js
@@ -0,0 +1,11 @@
+function printContact(){
+     let personInfo = {
+        name: 'Desiree',
+        address: '123 Main St',
+        city: 'Livingston',
+        state: 'NJ',
+        zip: '08500',
+    }
+    console.log(personInfo.name, personInfo.address, personInfo.city, personInfo.state, personInfo.zip)
+}
+printContact();
